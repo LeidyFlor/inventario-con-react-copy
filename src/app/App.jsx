84717f22547683c962b173659d991bf23fe15dd5
Miller Guerrero <mileralejandroguerrero@gmail.com>
2026-04-04 02:20:31 -0500

    Se comentaron las páginas en App.jsx donde están ubicadas las rutas para poder visualizar los avances de cadaformulario

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,25 +1,17 @@
 import Input from "../shared/components/Input.jsx"
 import { CreateUserPage } from "@/features/users";
 import { CreteBrandPage } from "../features/brands/index.js";
-<<<<<<< HEAD
 import { CreateLoanPage } from "../features/loans/index.js";
 import { CreatePermissionPage } from "../features/permissions/index.js";
-=======
->>>>>>> 1e0fff203aba5ba74920f3cf44db478783d6fc0d
 
 export default function App(){
 
     return(
         <div className="min-h-screen flex items-center justify-center">
-<<<<<<< HEAD
         {/* <CreateUserPage /> */}
         {/* <CreteBrandPage /> */}
         {/* <CreateLoanPage /> */}
         <CreatePermissionPage />
-=======
-        <CreateUserPage />
-        {/* <CreteBrandPage /> */}
->>>>>>> 1e0fff203aba5ba74920f3cf44db478783d6fc0d
     </div>
     );
 };
