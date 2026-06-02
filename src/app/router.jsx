e1dbd7541e7de8d5caec075e73114a109559aa90
tefalover <tefania0917@gmail.com>
2026-06-02 10:22:57 -0500

    feat(consumable-material): implementacion de rutas(visualizar, listar)

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -4,13 +4,12 @@ import { CreateUserPage, EditUserPage, ListUserPage, ViewUserPage } from "@/feat
 import { CreateBrandPage, ListBrandPage } from "@/features/brands";
 import { CreateLoanPage, ReturnLoan, ApproveReturnLoan, ListLoanPage, ViewLoanPage } from "@/features/loans";
 import { CreateTaskPage } from "@/features/tasks";
-import { CreateConsumablePage, EditConsumablePage, ListMaterialPage, ViewMaterialPage} from "@/features/consumable-material";
+import { CreateConsumablePage, EditConsumablePage, ListMaterialPage, ViewMaterialPage } from "@/features/consumable-material";
 import { CreateReturnablePage, EditReturnablePage, ListReturnablePage, ViewReturnablePage } from "@/features/returnable-material";
 import { ListPermissionsPage } from "@/features/permissions";
 import { LoginForm, LoginRestorePassword, LoginRestorePasswordCode, LoginRestoreNewPassword } from "@/features/auth";
 import { AuthLayout, DashboardLayout } from "@/shared/";
 import { HomePage } from "@/features/home";
-
 
 
 const router = createBrowserRouter([
@@ -40,13 +39,22 @@ const router = createBrowserRouter([
             { path: "user-create", element: <CreateUserPage /> },
             { path: "user-list", element: <ListUserPage /> }, 
             { path: "users/:id/edit", element: <EditUserPage /> },
-            { path: "users/:id/view", element: <ViewUserPage /> },
-            { path: "materials/:id/edit", element: <h1>Editar material</h1> },
-            { path: "materials/:id/view", element: <h1>Ver material</h1> }, 
+            { path: "users/:id/view", element: <ViewUserPage /> }, 
             { path: "returnable-material-create", element: <CreateReturnablePage /> },
             { path: "returnable-material-list", element: <ListReturnablePage /> },
             { path: "returnable-materials/:id/view", element: <ViewReturnablePage /> },
             { path: "returnable-materials/:id/edit", element: <h1>Ver material devolutivo</h1> },
+            
+            // Material de consumo
+            { path: "consumable-material-create", element: <CreateConsumablePage /> },
+            { path: "consumable-material-list", element: <ListMaterialPage
+             /> }, 
+            { path: "materials/:id/edit", element: <EditConsumablePage/> },
+            { path: "materials/:id/view", element: <ViewMaterialPage/> },
+            
+
+
+
             { path: "loan-create", element: <CreateLoanPage /> },
             { path: "loan-list", element: <ListLoanPage /> },
             { path: "loan-editar", element: <h1>Editar prestamo</h1> },
