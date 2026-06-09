41b42fdd197b23bf112babfdf7d15aa9bf93cb0e
tefalover <tefania0917@gmail.com>
2026-06-09 10:03:34 -0500

    feat(brands): implementació de el modal al dar clic en crear marca

--- src/features/brands/components/BrandRegisterForm.jsx
+++ src/features/brands/components/BrandRegisterForm.jsx
@@ -1,19 +1,14 @@
 import { Input, Button, IconButton } from "@/shared"
 import { Tags } from "lucide-react";
-import { Link } from "react-router-dom"
 
-export default function BrandRegisterForm(){
+export default function BrandRegisterForm({ onClose }) {
     const handleBrand = (e) => {
         console.log("Marca: ", e.target.value);
     }
 
     return(
-        <div className="flex flex-col items-center justify-center min-h-screen relative">
-
-            {/* Contenedor verde */}
+        <div className="flex flex-col items-center justify-center relative">
             <div className="bg-gradient-container-green border-4 border-border-green-container p-6 rounded-4xl w-fit place-self-center">
-
-                {/* Título con línea degradada */}
                 <div className="mb-6 max-w-max">
                     <h1 className="flex gap-2 text-gradient-title text-h3 pb-0.5">
                         <Tags className="text-brand" />
@@ -22,7 +17,6 @@ export default function BrandRegisterForm(){
                     <div className="h-0.5 bg-gradiant-title-line"></div>
                 </div>
 
-                {/* Contenido centrado */}
                 <div className="flex flex-col items-center gap-5">
                     <Input
                         placeholder="Nombre de la marca"
@@ -30,15 +24,12 @@ export default function BrandRegisterForm(){
                         onChange={handleBrand}
                         name="brandName"
                     />
-                    <Link to="/dashboard/brand-list">
-                        <IconButton>
-                            Aceptar
-                        </IconButton>
-                    </Link>
+                    {/* Llama onClose en vez de navegar */}
+                    <IconButton onClick={onClose}>
+                        Aceptar
+                    </IconButton>
                 </div>
-
             </div>
-
         </div>
     );
 }
