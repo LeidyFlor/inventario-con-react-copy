e62ef3261cfce5463ab4f1f41258d311452b5159
Leidy <leidyflor2001@gmail.com>
2026-07-25 18:41:45 -0500

    style(auth): Se agregan imagenes con fondos para resoluciones mejores a desktop, como tablet y mobil, esto son aproximados, entonces en algunos celulares o tablets puede cortarse la imagen

--- frontend/src/shared/layouts/AuthLayout.jsx
+++ frontend/src/shared/layouts/AuthLayout.jsx
@@ -1,5 +1,7 @@
 import { Outlet } from "react-router-dom";
 import authBg from "@/assets/images/auth-bg.jpg";
+import authBgMobil from "@/assets/images/auth-bg-mobil.png";
+import authBgTablet from "@/assets/images/auth-bg-tablet.png";
 import { CreateUserPage } from "@/features/users";
 import { LoginForm } from "@/features/auth";
 
@@ -7,9 +9,20 @@ export default function AuthLayout() {
     return (
         <div className="relative min-h-screen text-text-primary">
             {/* Fondo con imagen */}
-            <div className="absolute inset-0 -z-10 bg-cover bg-center"
-                style={{ backgroundImage: `url(${authBg})` }}
-            />
+            <div className="absolute inset-0 -z-10">
+                {/* Móvil */}
+                <div className="block md:hidden absolute inset-0 bg-cover bg-center"
+                    style={{ backgroundImage: `url(${authBgMobil})` }}
+                />
+                {/* Tablet */}
+                <div className="hidden md:block lg:hidden absolute inset-0 bg-cover bg-center"
+                    style={{ backgroundImage: `url(${authBgTablet})` }}
+                />
+                {/* Desktop */}
+                <div className="hidden lg:block absolute inset-0 bg-cover bg-center"
+                    style={{ backgroundImage: `url(${authBg})` }}
+                />
+            </div>
             {/* outlet inyecta los elemntos hijos de las routes */}
             <main>
 
